aeab659b2f598d165aca661f26e5aff583956bf4
omar-ameen1 <mierzofdoom@gmail.com>
2022-06-07 12:12:23 +0100

    Added primitive Metamask account login. Can currently prompt Metamask login and will display wallet address as a string in the extension.

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,25 +1,28 @@
 window.userWalletAddress = null
 const loginButton = document.getElementById('loginButton')
 const userWallet = document.getElementById('userWallet')
+const createProvider = require('metamask-extension-provider')
+const provider = createProvider()
+const Web3 = require('web3');
 
 function toggleButton() {
-    if (!window.ethereum) {
+    if (!provider) {
         loginButton.innerText = 'MetaMask is not installed'
         loginButton.classList.remove('bg-purple-500', 'text-white')
         loginButton.classList.add('bg-gray-500', 'text-gray-100', 'cursor-not-allowed')
         return false
     }
-
     loginButton.addEventListener('click', loginWithMetaMask)
 }
 
 async function loginWithMetaMask() {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-        .catch((e) => {
-            console.error(e.message)
-        })
+    const web3 = new Web3(provider)
+    const accounts = await Promise.all([
+        provider.request({
+            method: 'eth_requestAccounts',
+        }),
+    ])
     if (!accounts) { return }
-
     window.userWalletAddress = accounts[0]
     userWallet.innerText = window.userWalletAddress
     loginButton.innerText = 'Sign out of MetaMask'
