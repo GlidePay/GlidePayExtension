5cd3f28b2054c0964a51bf30b6c05daa70e89b7c
omar-ameen1 <mierzofdoom@gmail.com>
2022-06-17 16:36:41 -0400

    Began styling cart confirmation.

--- scripts/background.js
+++ scripts/background.js
@@ -12,13 +12,13 @@ chrome.runtime.onMessage.addListener((message, sender, response) => {
                 // TODO: Make sure order popup has a confirm button that sends out a message from windowedpopup
                 //  with message promptTransaction
                 let top = 0;
-                let left = message.screenSize - 360; //300 is width of extension
+                let left = message.screenSize - 720;
                 try {
                     const lastFocused = chrome.getLastFocused()
                     top = lastFocused.top;
                 } catch (e) {
                 }
-                chrome.windows.create({url:"views/popup.html", type: "popup", top: top, left: left,
+                chrome.windows.create({url:"views/confirmation.html", type: "popup", top: top, left: left,
                     width: 360, height: 620}, (window) => {
                 });
             } break;
