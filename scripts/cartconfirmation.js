8e2ab627d4ff8d24969b69585d75c73885a5aea5
Marc Eftimie <marc.eftimie@gmail.com>
2022-07-08 22:30:56 -0400

    Merged UI changes

--- scripts/cartconfirmation.js
+++ scripts/cartconfirmation.js
@@ -42,27 +42,60 @@ function getAddresses(userid) {
 }
 
 const setProductInfo = (products) => {
-  console.log("Receceived product info");
   console.log(JSON.stringify(products));
   let i = 0;
   const productSection = document.getElementById("productInfo");
-  let totalPrice = 0;
+  let totalprice = 0;
+  console.log(typeof products);
+  console.log(products);
+  console.log(Object.keys(products));
+  if (Object.keys(products).length > 0) {
+    const horizontal_divider = document.createElement("hr");
+    productSection.appendChild(horizontal_divider);
+  }
   for (let value in products) {
-    totalPrice += parseFloat(products[value][1]) * parseInt(products[value][0]);
-    const image = document.createElement("img");
-    image.src = products[value][2];
-    const title = document.createElement("p");
-    title.textContent = products[value][0];
-    image.setAttribute("height", "100px");
-    image.setAttribute("width", "100px");
-    productSection.appendChild(image);
-    productSection.appendChild(title);
+    const cartItem = document.createElement("div");
+    totalprice += parseFloat(products[value][1]) * parseInt(products[value][0]);
+
+    const itemImageColumn = document.createElement("col-md-4");
+    const itemImage = document.createElement("img");
+    itemImage.src = products[value][2];
+    itemImage.setAttribute("height", "100px");
+    itemImage.setAttribute("width", "100px");
+    itemImageColumn.appendChild(itemImage);
+
+    const itemNameColumn = document.createElement("col-md-4");
+    // const itemName = document.createElement("h2");
+    itemNameColumn.textContent = products[value][3];
+    // itemNameColumn.appendChild(itemImage);
+
+    const itemPriceColumn = document.createElement("col-md-4");
+    // const itemPrice = document.createElement("h2");
+    itemPriceColumn.textContent = `$${products[value][1]}`;
+    // itemPrice.classList.add("pull-right");
+    // itemPriceColumn.appendChild(itemImage);
+
+    const itemQuantity = document.createElement("h2");
+    itemQuantity.textContent = `Qty: ${products[value][0]}`;
+
+    const horizontal_divider = document.createElement("hr");
+    const cellRow1 = document.createElement("div");
+    cellRow1.setAttribute("class", "bg-light d-flex justify-content-between");
+    cellRow1.appendChild(itemImageColumn);
+    cellRow1.appendChild(itemNameColumn);
+    cellRow1.appendChild(itemPriceColumn);
+    cartItem.appendChild(cellRow1);
+    cartItem.appendChild(itemQuantity);
+    cartItem.appendChild(horizontal_divider);
+    productSection.appendChild(cartItem);
     i++;
   }
   const confirmButton = document.createElement("button");
+  confirmButton.setId;
   confirmButton.setAttribute("class", "btn btn-primary");
   confirmButton.textContent = "Confirm Order";
   confirmButton.addEventListener("click", () => {
+    lambdaTest();
     chrome.windows.getAll({ populate: true }, (windows) => {
       for (let a in windows) {
         for (let b in windows[a].tabs) {
@@ -70,17 +103,13 @@ const setProductInfo = (products) => {
             chrome.tabs.sendMessage(windows[a].tabs[b].id, {
               from: "popup",
               subject: "promptTransaction",
-              price: totalPrice,
-              //address: document.getElementById('addressSelect').value
-              products: products,
-              addressid: "1", //TODO: Get address id from select
+              price: totalprice,
             });
             break;
           }
         }
       }
     });
-    //TODO: This needs to pass order info to the promptTransaction function
   });
   chrome.storage.session.get("userid", function (result) {
     console.log(result["userid"]);
@@ -89,6 +118,20 @@ const setProductInfo = (products) => {
   });
   productSection.appendChild(confirmButton);
 };
+
+function lambdaTest() {
+  fetch(
+    "https://a5w54in31c.execute-api.us-east-1.amazonaws.com/default/omarpython",
+    {
+      method: "post",
+    }
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data);
+      //use keys
+    });
+}
 
 window.addEventListener("load", () => {
   chrome.windows.getAll({ populate: true }, (windows) => {
@@ -106,3 +149,23 @@ window.addEventListener("load", () => {
     }
   });
 });
+
+////
+
+// window.addEventListener("load", () => {
+//   chrome.windows.getAll({ populate: true }, (windows) => {
+//     for (let a in windows) {
+//       for (let b in windows[a].tabs) {
+//         if (windows[a].tabs[b].url.includes("amazon.com/gp/cart")) {
+//           console.log("hi");
+//           chrome.tabs.sendMessage(
+//             windows[a].tabs[b].id,
+//             { from: "popup", subject: "needInfo" },
+//             setProductInfo
+//           );
+//           break;
+//         }
+//       }
+//     }
+//   });
+// });
